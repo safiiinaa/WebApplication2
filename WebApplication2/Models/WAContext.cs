5a4571d28cfffa6687b35e24b85f52df85de2c84
Гульнур Сафина <lafleur300997@gmail.com>
2020-09-07 21:56:23 +0500

    Добавьте файлы проекта.

---
+++ WebApplication2/Models/WAContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WA.Models
+{
+    public class WAContext : DbContext
+    {
+        public WAContext(DbContextOptions<WAContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WAItem> Items { get; set; }
+    }
+}
